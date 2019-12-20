2bd76fc302c8254393452847e3b81ee629f16019
joshIsCoding <yes_you_do@hotmail.com>
2019-12-20 20:33:46 +0000

    Add Cat#age method calculated using birth_date

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,5 @@
 class Cat < ApplicationRecord
+   include ActionView::Helpers::DateHelper
    COLORS = [
       "tortoiseshell",
       "tabby",
@@ -7,4 +8,8 @@ class Cat < ApplicationRecord
    validates :birth_date, :name, presence: true
    validates :color, inclusion: { in: COLORS }, presence: true 
    validates :sex, inclusion: { in: ["M", "F"] }, presence: true
+
+   def age
+      time_ago_in_words(birth_date)
+   end
 end
