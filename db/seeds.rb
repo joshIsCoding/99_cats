005f1f0ed623437c43a0007e28ff23f546e27991
joshIsCoding <yes_you_do@hotmail.com>
2019-12-20 22:29:39 +0000

    Seed the database with a few cat objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cats = Cat.create([
+   { name: "Sennacy", birth_date: "01/07/2015", color: "leopard", sex: "M", description: "I cherish this little panther." },
+   { name: "Tabitha", birth_date: "10/02/2016", color: "tabby", sex: "F", description: "Charmed, I'm sure." },
+   { name: "Willace", birth_date: "23/11/2017", color: "tortoiseshell", sex: "M", description: "Truly, an aristocat." },
+])
