bf999a8d48364bfd509bdd104632bc234619af53
joshIsCoding <yes_you_do@hotmail.com>
2019-12-20 22:35:48 +0000

    Add the Cat#show action

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,13 @@ class CatsController < ApplicationController
       @cats = Cat.all
       render :index
    end
+
+   def show
+      @cat = Cat.find_by_id(params[:id])
+      if @cat
+         render :show
+      else
+         redirect_to cats_url
+      end
+   end
 end
