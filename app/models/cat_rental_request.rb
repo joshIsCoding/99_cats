c3f5d84c437ef445e1b2ea0feeffebee33b84b90
joshIsCoding <yes_you_do@hotmail.com>
2019-12-21 23:47:51 +0000

    Write #overlapping_requests and #overlapping_approved_requests queries

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -8,4 +8,19 @@ class CatRentalRequest < ApplicationRecord
       foreign_key: :cat_id,
       primary_key: :id
    )
+
+   def overlapping_requests
+      CatRentalRequest.where(cat_id: self.cat_id)
+      .where.not(<<-SQL, this_start_date: self.start_date, this_end_date: self.end_date, this_id: self.id)
+         (end_date < :this_start_date)
+         OR (start_date > :this_end_date)
+         OR (id = :this_id)
+      SQL
+      # filters out all rental requests that start after or end before the current 
+      # request for the same cat
+   end
+
+   def overlapping_approved_requests
+      overlapping_requests.where(status: "APPROVED")
+   end
 end
