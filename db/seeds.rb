0f0c03f6e3df4af589b93fe99fc0472190b87ef3
joshIsCoding <yes_you_do@hotmail.com>
2019-12-21 23:47:23 +0000

    Add CatRentalRequest seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,14 @@ cats = Cat.create([
    { name: "Tabitha", birth_date: "10/02/2016", color: "tabby", sex: "F", description: "Charmed, I'm sure." },
    { name: "Willace", birth_date: "23/11/2017", color: "tortoiseshell", sex: "M", description: "Truly, an aristocat." },
 ])
+
+cat_rental_requests = CatRentalRequest.create([
+   { cat_id: 3, start_date: "05/01/2020", end_date: "10/01/2020"},
+   { cat_id: 3, start_date: "01/01/2020", end_date: "5/01/2020"},
+   { cat_id: 3, start_date: "06/01/2020", end_date: "7/01/2020", status: "APPROVED"},
+   { cat_id: 3, start_date: "07/01/2020", end_date: "11/01/2020"},
+   { cat_id: 3, start_date: "10/01/2020", end_date: "15/01/2020"},
+   { cat_id: 3, start_date: "11/01/2020", end_date: "15/01/2020"},
+   { cat_id: 3, start_date: "1/01/2020", end_date: "11/01/2020"},
+   { cat_id: 3, start_date: "1/01/2020", end_date: "3/01/2020"}
+])
