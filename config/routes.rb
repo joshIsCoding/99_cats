00e9dd8364703f378a2cc57ebc80760fbd72a530
joshIsCoding <yes_you_do@hotmail.com>
2019-12-22 18:57:56 +0000

    Create approve and deny member routes for cat_rental_requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats
-  resources :cat_rental_requests, only: [:new, :create, :show]
+  resources :cat_rental_requests, only: [:new, :create, :show] do
+    member do 
+      patch 'approve'
+      patch 'deny'
+    end
+  end
 end
