aff87903b78c70e9542177b1e555fa3a88402a06
joshIsCoding <yes_you_do@hotmail.com>
2019-12-23 22:46:30 +0000

    Add User::generate_session_token and #reset_session_token! methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,16 @@ class User < ApplicationRecord
    
    #after_initialize :ensure_session_token_set
 
+   def self.generate_session_token
+      SecureRandom::urlsafe_base64(16)
+   end
    
+   def reset_session_token!
+      self.session_token = self.class.generate_session_token
+      self.save!
+      self.session_token
+   end
+
 
 
 end
