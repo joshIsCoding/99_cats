3bb90779a81d49742799e201358c590a254b5c27
joshIsCoding <yes_you_do@hotmail.com>
2019-12-25 21:35:40 +0000

    Generate and run migration to ensure cats.user_id presence

---
+++ db/migrate/20191225203042_change_user_id_to_ensure_presence.rb
@@ -0,0 +1,9 @@
+class ChangeUserIdToEnsurePresence < ActiveRecord::Migration[5.2]
+  def up
+    change_column_null :cats, :user_id, false 
+  end
+
+  def down
+    change_column_null :cats, :user_id, true
+  end
+end
