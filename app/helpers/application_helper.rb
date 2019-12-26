14424110898f8871d347a814145dd319bf10dbf4
joshIsCoding <yes_you_do@hotmail.com>
2019-12-26 21:11:40 +0000

    Write a CSRF prevention helper method #auth_token to embed a hidden input for the token in forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+   def auth_token
+      input_string = "<input "
+      input_string += "type=\"hidden\" "
+      input_string += "name=\"authenticity_token\" "
+      input_string += "value=\"#{form_authenticity_token}\" />"
+      input_string.html_safe
+   end
 end
